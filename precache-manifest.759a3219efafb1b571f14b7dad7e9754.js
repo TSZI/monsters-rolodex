38e6d67d53c8bc62c1ab522c510ce11e44ef2a3e
Troy Szatkowski <troy.m.szatkowski@gmail.com>
2019-10-26 16:01:23 -0400

    Updates

---
+++ precache-manifest.759a3219efafb1b571f14b7dad7e9754.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3631788680ecab8eb71cc5ee49ca9abf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1944d311cd9d80b60bf5",
+    "url": "/monsters-rolodex/static/css/main.fc65a6b5.chunk.css"
+  },
+  {
+    "revision": "b0b58fe607faa4d03a43",
+    "url": "/monsters-rolodex/static/js/2.de1aa811.chunk.js"
+  },
+  {
+    "revision": "1944d311cd9d80b60bf5",
+    "url": "/monsters-rolodex/static/js/main.bfa11293.chunk.js"
+  },
+  {
+    "revision": "1b1f57fd6c4b40aa49d9",
+    "url": "/monsters-rolodex/static/js/runtime-main.10abe5f5.js"
+  }
+]);
